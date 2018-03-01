e63148c547f6106ce7b1128cb2f9b5ce9b273bbd
Chris Marchesi <chrism@vancluevertech.com>
2018-02-28 17:05:36 -0800

    d/public_key: Move test to more standard naming convention
    The resources in this provider are not following the convention, so this
    is no fault of anyone's, but might as well work towards getting
    consistent, especially so that we can better distinguish between
    resource and data source.

--- tls/data_source_public_key_test.go
+++ tls/data_source_public_key_test.go
@@ -18,12 +18,12 @@ D9Hk2MajZuFnJiqj1QIDAQAB
 	expectedPublicSSH = `ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDPLaq43D9C596ko9yQipWUf2FbRhFs18D3wBDBqXLIoP7W3rm5S292/JiNPa+mX76IYFF416zTBGG9J5w4d4VFrROn8IuMWqHgdXsCUf2szN7EnJcVBsBzTxxWqz4DjX315vbm/PFOLlKzC0Ngs4h1iDiCD9Hk2MajZuFnJiqj1Q==`
 )
 
-func TestAccPublicKey_dataSource(t *testing.T) {
+func TestAccDataSourceTLSPublicKey_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		Providers: testProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: fmt.Sprintf(testAccDataSourcePublicKeyConfig, testPrivateKey),
+				Config: fmt.Sprintf(testAccDataSourceTLSPublicKeyConfig, testPrivateKey),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_pem", strings.TrimSpace(expectedPublic)+"\n"),
 					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_openssh", strings.TrimSpace(expectedPublicSSH)+"\n"),
@@ -55,7 +55,7 @@ func TestAccPublicKey_dataSource(t *testing.T) {
 	})
 }
 
-const testAccDataSourcePublicKeyConfig = `
+const testAccDataSourceTLSPublicKeyConfig = `
 data "tls_public_key" "test" {
   private_key_pem = <<EOF
 	%s
