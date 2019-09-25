59d264bfc2d5e090c983d663aef9b31d8c2e3991
Alex Pilon <apilon@hashicorp.com>
2019-09-25 16:36:24 -0400

    switch to standalone tf plugin sdk

--- tls/resource_cert_request_test.go
+++ tls/resource_cert_request_test.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"testing"
 
-	r "github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	r "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestCertRequest(t *testing.T) {
--- tls/resource_private_key_test.go
+++ tls/resource_private_key_test.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 	"testing"
 
-	r "github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	r "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestPrivateKeyRSA(t *testing.T) {
