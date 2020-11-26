d5b6e8f55c1197687b7783b563602450d26aeaf0
Chris Marchesi <chrism@vancluevertech.com>
2020-11-26 11:22:57 -0800

    Fix package to follow correct Go version guidelines
    The current package name
    (github.com/terraform-providers/terraform-provider-tls) lacks a version
    path and as such cannot be imported into other projects.
    
    I currently depend on this provider in
    github.com/vancluever/terraform-provider-acme, and this is effectively
    keeping me from modernizing the provider due to a lack of ability to
    upgrade testing dependencies.

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/terraform-providers/terraform-provider-tls
+module github.com/terraform-providers/terraform-provider-tls/v3
 
 go 1.14
 
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/terraform-providers/terraform-provider-tls/internal/provider"
+	"github.com/terraform-providers/terraform-provider-tls/v3/internal/provider"
 )
 
 func main() {
