419db34c9c88ea96978877ed2a92d131bc13ea71
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-02-21 16:42:11 +0000

    Removing dependency on `testify` as requested by Katy Moe

--- go.mod
+++ go.mod
@@ -4,6 +4,5 @@ go 1.16
 
 require (
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.10.1
-	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
 )
--- internal/openssh/lib_test.go
+++ internal/openssh/lib_test.go
@@ -9,71 +9,90 @@ import (
 	"encoding/pem"
 	"golang.org/x/crypto/ssh"
 	"testing"
-
-	testifyAssert "github.com/stretchr/testify/assert"
 )
 
 func TestOpenSSHFormat_MarshalAndUnmarshal_RSA(t *testing.T) {
-	assert := testifyAssert.New(t)
-
 	// Given an RSA private key
 	rsaOrig, err := rsa.GenerateKey(rand.Reader, 4096)
-	assert.NoError(err)
+	if err != nil {
+		t.Errorf("Failed to generate RSA private key: %v", err)
+	}
 
 	// Marshal it to OpenSSH PEM format
 	pemOpenSSHPrvKey, err := MarshalPrivateKey(rsaOrig, "")
-	assert.NoError(err)
+	if err != nil {
+		t.Errorf("Failed to marshall RSA private key to OpenSSH PEM: %v", err)
+	}
 	pemOpenSSHPrvKeyBytes := pem.EncodeToMemory(pemOpenSSHPrvKey)
 
 	// Parse it back into an RSA private key
 	rawPrivateKey, err := ssh.ParseRawPrivateKey(pemOpenSSHPrvKeyBytes)
 	rsaParsed, ok := rawPrivateKey.(*rsa.PrivateKey)
-	assert.True(ok)
+	if !ok {
+		t.Errorf("Failed to type assert RSA private key: %v", rawPrivateKey)
+	}
 
 	// Confirm RSA is valid
-	assert.NoError(rsaParsed.Validate())
+	err = rsaParsed.Validate()
+	if err != nil {
+		t.Errorf("Parsed RSA private key is not valid: %v", err)
+	}
 	// Confirm it matches the original key by comparing the public ones
-	assert.True(rsaParsed.Equal(rsaOrig))
+	if !rsaParsed.Equal(rsaOrig) {
+		t.Errorf("Parsed RSA private key doesn't match the original")
+	}
 }
 
 func TestOpenSSHFormat_MarshalAndUnmarshal_ECDSA(t *testing.T) {
-	assert := testifyAssert.New(t)
-
 	// Given an ECDSA private key
 	ecdsaOrig, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
-	assert.NoError(err)
+	if err != nil {
+		t.Errorf("Failed to generate ECDSA private key: %v", err)
+	}
 
 	// Marshal it to OpenSSH PEM format
 	pemOpenSSHPrvKey, err := MarshalPrivateKey(ecdsaOrig, "")
-	assert.NoError(err)
+	if err != nil {
+		t.Errorf("Failed to marshall ECDSA private key to OpenSSH PEM: %v", err)
+	}
 	pemOpenSSHPrvKeyBytes := pem.EncodeToMemory(pemOpenSSHPrvKey)
 
 	// Parse it back into an ECDSA private key
 	rawPrivateKey, err := ssh.ParseRawPrivateKey(pemOpenSSHPrvKeyBytes)
 	ecdsaParsed, ok := rawPrivateKey.(*ecdsa.PrivateKey)
-	assert.True(ok)
+	if !ok {
+		t.Errorf("Failed to type assert ECDSA private key: %v", rawPrivateKey)
+	}
 
 	// Confirm it matches the original key by comparing the public ones
-	assert.True(ecdsaParsed.Equal(ecdsaOrig))
+	if !ecdsaParsed.Equal(ecdsaOrig) {
+		t.Errorf("Parsed ECDSA private key doesn't match the original")
+	}
 }
 
 func TestOpenSSHFormat_MarshalAndUnmarshal_ED25519(t *testing.T) {
-	assert := testifyAssert.New(t)
-
 	// Given an ED25519 private key
 	_, ed25519Orig, err := ed25519.GenerateKey(rand.Reader)
-	assert.NoError(err)
+	if err != nil {
+		t.Errorf("Failed to generate ED25519 private key: %v", err)
+	}
 
 	// Marshal it to OpenSSH PEM format
 	pemOpenSSHPrvKey, err := MarshalPrivateKey(ed25519Orig, "")
-	assert.NoError(err)
+	if err != nil {
+		t.Errorf("Failed to marshall ED25519 private key to OpenSSH PEM: %v", err)
+	}
 	pemOpenSSHPrvKeyBytes := pem.EncodeToMemory(pemOpenSSHPrvKey)
 
 	// Parse it back into an ED25519 private key
 	rawPrivateKey, err := ssh.ParseRawPrivateKey(pemOpenSSHPrvKeyBytes)
 	ed25519Parsed, ok := rawPrivateKey.(*ed25519.PrivateKey)
-	assert.True(ok)
+	if !ok {
+		t.Errorf("Failed to type assert ED25519 private key: %v", rawPrivateKey)
+	}
 
 	// Confirm it matches the original key by comparing the public ones
-	assert.True(ed25519Parsed.Equal(ed25519Orig))
+	if !ed25519Parsed.Equal(ed25519Orig) {
+		t.Errorf("Parsed ED25519 private key doesn't match the original")
+	}
 }
