b52dd9c1d34c8fd036209a855f6ac76e2dbbc388
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-03-03 19:32:45 +0000

    Moving test fixtures out of `provider_test.go` and into `fixtures_test.go`

--- internal/provider/data_source_public_key_test.go
+++ internal/provider/data_source_public_key_test.go
@@ -10,14 +10,20 @@ import (
 )
 
 const (
-	expectedPublic = `-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDPLaq43D9C596ko9yQipWUf2Fb
-RhFs18D3wBDBqXLIoP7W3rm5S292/JiNPa+mX76IYFF416zTBGG9J5w4d4VFrROn
-8IuMWqHgdXsCUf2szN7EnJcVBsBzTxxWqz4DjX315vbm/PFOLlKzC0Ngs4h1iDiC
-D9Hk2MajZuFnJiqj1QIDAQAB
------END PUBLIC KEY-----`
-	expectedPublicSSH            = `ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDPLaq43D9C596ko9yQipWUf2FbRhFs18D3wBDBqXLIoP7W3rm5S292/JiNPa+mX76IYFF416zTBGG9J5w4d4VFrROn8IuMWqHgdXsCUf2szN7EnJcVBsBzTxxWqz4DjX315vbm/PFOLlKzC0Ngs4h1iDiCD9Hk2MajZuFnJiqj1Q==`
-	expectedPublicFingerprintMD5 = `62:c2:c6:7a:d0:27:72:e7:0d:bc:4e:97:42:0e:9e:e6`
+	configDataSourcePublicKeyViaPEM = `
+data "tls_public_key" "test" {
+	private_key_pem = <<EOF
+	%s
+	EOF
+}
+`
+	configDataSourcePublicKeyViaOpenSSHPEM = `
+data "tls_public_key" "test" {
+	private_key_openssh = <<EOF
+	%s
+	EOF
+}
+`
 )
 
 func TestAccPublicKey_dataSource(t *testing.T) {
@@ -25,7 +31,7 @@ func TestAccPublicKey_dataSource(t *testing.T) {
 		Providers: testProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(testAccDataSourcePublicKeyConfig, testPrivateKey),
+				Config: fmt.Sprintf(configDataSourcePublicKeyViaPEM, testPrivateKeyPEM),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_pem", strings.TrimSpace(expectedPublic)+"\n"),
 					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_openssh", strings.TrimSpace(expectedPublicSSH)+"\n"),
@@ -67,10 +73,4 @@ func TestAccPublicKey_dataSource(t *testing.T) {
 	})
 }
 
-const testAccDataSourcePublicKeyConfig = `
-data "tls_public_key" "test" {
-	private_key_pem = <<EOF
-	%s
-	EOF
 }
-`
