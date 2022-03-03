06d74034a56d8d4456a6f34a7d323ffd5bed730f
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-03-03 19:38:16 +0000

    Adding support for `ED25519` keys in `data.tls_public_key`, as well as ability to initialise it using `private_key_openssh` instead of `private_key_pem`
    The arguments `private_key_pem` and `private_key_openssh` are mutually exclusive. I'm adding this to make this Data Source symmetrical to the `tls_private_key` resource.

--- internal/provider/data_source_public_key.go
+++ internal/provider/data_source_public_key.go
@@ -1,27 +1,35 @@
 package provider
 
 import (
+	"crypto"
 	"encoding/pem"
 	"fmt"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"golang.org/x/crypto/ssh"
 )
 
 func dataSourcePublicKey() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourcePublicKeyRead,
+		Read: readDataSourcePublicKey,
 		Schema: map[string]*schema.Schema{
 			"private_key_pem": {
 				Type:        schema.TypeString,
-				Required:    true,
+				Optional:    true,
 				Sensitive:   true,
-				Description: "PEM formatted string to use as the private key",
+				Description: "Private key data in PEM format; either this or `private_key_openssh` must be set",
+			},
+
+			"private_key_openssh": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				Description: "Private key data in OpenSSH-compatible PEM format; either this or `private_key_pem` must be set",
 			},
 
 			"algorithm": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "Name of the algorithm to use to generate the private key",
+				Description: "Name of the algorithm used to generate the private key",
 			},
 
 			"public_key_pem": {
@@ -51,43 +59,73 @@ func dataSourcePublicKey() *schema.Resource {
 	}
 }
 
-func dataSourcePublicKeyRead(d *schema.ResourceData, _ interface{}) error {
-	// Read private key
-	var bytes []byte
-	if v, ok := d.GetOk("private_key_pem"); ok {
-		bytes = []byte(v.(string))
-	} else {
-		return fmt.Errorf("invalid private key %#v", v)
+func readDataSourcePublicKey(d *schema.ResourceData, _ interface{}) error {
+	prvKeyPEMArg, prvKeyPEMArgOK := d.GetOk("private_key_pem")
+	prvKeyOpenSSHPEMArg, prvKeyOpenSSHPEMArgOK := d.GetOk("private_key_openssh")
+
+	// Confirm that not both ways to provide a private key were used at the same time
+	if prvKeyPEMArgOK && prvKeyOpenSSHPEMArgOK {
+		return fmt.Errorf("either provide private key via `private_key_pem` or `private_key_openssh`, not both")
 	}
-	// decode PEM encoding to ANS.1 PKCS1 DER
-	keyPemBlock, _ := pem.Decode(bytes)
 
-	if keyPemBlock == nil || (keyPemBlock.Type != "RSA PRIVATE KEY" && keyPemBlock.Type != "EC PRIVATE KEY") {
-		typ := "unknown"
+	// First, attempt to read private key from `private_key_pem` argument (PEM format)
+	if prvKeyPEMArgOK {
+		prvKeyPEMBytes := []byte(prvKeyPEMArg.(string))
 
-		if keyPemBlock != nil {
-			typ = keyPemBlock.Type
+		prvKey, err := privateKeyFromPEM(d, prvKeyPEMBytes)
+		if err != nil {
+			return err
 		}
 
-		return fmt.Errorf("failed to decode PEM block containing private key of type %#v", typ)
+		return setPublicKeyAttributes(d, prvKey)
 	}
 
-	keyAlgo := ""
-	switch keyPemBlock.Type {
-	case "RSA PRIVATE KEY":
-		keyAlgo = "RSA"
-	case "EC PRIVATE KEY":
-		keyAlgo = "ECDSA"
+	// Second, attempt to read private key from `private_key_openssh` argument (OpenSSH PEM format)
+	if prvKeyOpenSSHPEMArgOK {
+		prvKeyOpenSSHPEMBytes := []byte(prvKeyOpenSSHPEMArg.(string))
+
+		prvKey, err := ssh.ParseRawPrivateKey(prvKeyOpenSSHPEMBytes)
+		if err != nil {
+			return err
+		}
+
+		keyAlgorithm, err := PrivateKeyToAlgorithm(prvKey)
+		if err != nil {
+			return err
+		}
+		if err := d.Set("algorithm", keyAlgorithm); err != nil {
+			return fmt.Errorf("error setting value on key 'algorithm': %s", err)
+		}
+
+		return setPublicKeyAttributes(d, prvKey)
 	}
-	if err := d.Set("algorithm", keyAlgo); err != nil {
-		return fmt.Errorf("error setting value on key 'algorithm': %s", err)
+
+	return fmt.Errorf("no valid private key was provided via `private_key_pem` nor `private_key_openssh`")
+}
+
+func privateKeyFromPEM(d *schema.ResourceData, prvKeyPEMBytes []byte) (crypto.PrivateKey, error) {
+	// decode raw PEM bytes to the corresponding pem.Block encoded structure (ANS.1 PKCS1 DER)
+	keyPemBlock, keyRest := pem.Decode(prvKeyPEMBytes)
+	if keyPemBlock == nil {
+		keyRestLen := len(keyRest)
+		keyDecodedLen := len(prvKeyPEMBytes) - keyRestLen
+		return nil, fmt.Errorf("failed to decode raw PEM block: decoded bytes %d, undecoded %d", keyDecodedLen, keyRestLen)
+	}
+
+	// Map PEM Preamble of the Private Key to the corresponding Algorithm
+	keyAlgorithm, err := PEMPreamblePrivateKey(keyPemBlock.Type).Algorithm()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := d.Set("algorithm", keyAlgorithm); err != nil {
+		return nil, fmt.Errorf("error setting value on key 'algorithm': %s", err)
 	}
 
 	// Converts a private key from its ASN.1 PKCS#1 DER encoded form
-	key, err := parsePrivateKey(d, "private_key_pem", "algorithm")
+	prvKey, err := parsePrivateKey(d, "private_key_pem", "algorithm")
 	if err != nil {
-		return fmt.Errorf("error converting key to algo: %s - %s", keyAlgo, err)
+		return nil, fmt.Errorf("error converting key to algo: %s - %s", keyAlgorithm, err)
 	}
-
-	return readPublicKey(d, key)
+	return prvKey, nil
 }
--- internal/provider/data_source_public_key_test.go
+++ internal/provider/data_source_public_key_test.go
@@ -26,16 +26,17 @@ data "tls_public_key" "test" {
 `
 )
 
-func TestAccPublicKey_dataSource(t *testing.T) {
+func TestAccPublicKey_dataSource_PEM(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
 		Providers: testProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(configDataSourcePublicKeyViaPEM, testPrivateKeyPEM),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_pem", strings.TrimSpace(expectedPublic)+"\n"),
-					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_openssh", strings.TrimSpace(expectedPublicSSH)+"\n"),
-					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_fingerprint_md5", strings.TrimSpace(expectedPublicFingerprintMD5)),
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_pem", strings.TrimSpace(testPublicKeyPEM)+"\n"),
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_openssh", strings.TrimSpace(testPublicKeyOpenSSH)+"\n"),
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_fingerprint_md5", strings.TrimSpace(testPublicKeyOpenSSHFingerprintMD5)),
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "algorithm", "RSA"),
 				),
 			},
 			{
@@ -49,28 +50,100 @@ func TestAccPublicKey_dataSource(t *testing.T) {
 				`,
 				Check: resource.TestCheckResourceAttrPair(
 					"data.tls_public_key.test", "public_key_pem",
-					"tls_private_key.test", "public_key_pem"),
+					"tls_private_key.test", "public_key_pem",
+				),
 			},
 			{
 				Config: `
-					resource "tls_private_key" "key" {
+					resource "tls_private_key" "ecdsaPrvKey" {
 						algorithm   = "ECDSA"
 						ecdsa_curve = "P384"
 					}
-					data "tls_public_key" "pub" {
-						private_key_pem = "${tls_private_key.key.private_key_pem}"
+					data "tls_public_key" "ecdsaPubKey" {
+						private_key_pem = "${tls_private_key.ecdsaPrvKey.private_key_pem}"
 					}
 				`,
-				Check: resource.TestCheckResourceAttrPair(
-					"data.tls_public_key.pub", "public_key_pem",
-					"tls_private_key.key", "public_key_pem"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"data.tls_public_key.ecdsaPubKey", "public_key_pem",
+						"tls_private_key.ecdsaPrvKey", "public_key_pem",
+					),
+					resource.TestCheckResourceAttr("data.tls_public_key.ecdsaPubKey", "algorithm", "ECDSA"),
+				),
 			},
 			{
-				Config:      fmt.Sprintf(testAccDataSourcePublicKeyConfig, "corrupt"),
-				ExpectError: regexp.MustCompile("failed to decode PEM block containing private key of type \"unknown\""),
+				Config:      fmt.Sprintf(configDataSourcePublicKeyViaPEM, "corrupt"),
+				ExpectError: regexp.MustCompile("failed to decode raw PEM block: decoded bytes \\d, undecoded \\d"),
 			},
 		},
 	})
 }
 
+func TestAccPublicKey_dataSource_OpenSSHPEM(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		Providers: testProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(configDataSourcePublicKeyViaOpenSSHPEM, testPrivateKeyOpenSSHPEM),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_pem", strings.TrimSpace(testPublicKeyPEM)+"\n"),
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_openssh", strings.TrimSpace(testPublicKeyOpenSSH)+"\n"),
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "public_key_fingerprint_md5", strings.TrimSpace(testPublicKeyOpenSSHFingerprintMD5)),
+					resource.TestCheckResourceAttr("data.tls_public_key.test", "algorithm", "RSA"),
+				),
+			},
+			{
+				Config: `
+					resource "tls_private_key" "rsaPrvKey" {
+						algorithm = "RSA"
+					}
+					data "tls_public_key" "rsaPubKey" {
+						private_key_openssh = "${tls_private_key.rsaPrvKey.private_key_openssh}"
+					}
+				`,
+				Check: resource.TestCheckResourceAttrPair(
+					"data.tls_public_key.rsaPubKey", "public_key_openssh",
+					"tls_private_key.rsaPrvKey", "public_key_openssh",
+				),
+			},
+			{
+				Config: `
+					resource "tls_private_key" "ed25519PrvKey" {
+						algorithm   = "ED25519"
+					}
+					data "tls_public_key" "ed25519PubKey" {
+						private_key_openssh = "${tls_private_key.ed25519PrvKey.private_key_openssh}"
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"data.tls_public_key.ed25519PubKey", "public_key_openssh",
+						"tls_private_key.ed25519PrvKey", "public_key_openssh",
+					),
+					resource.TestCheckResourceAttr("data.tls_public_key.ed25519PubKey", "algorithm", "ED25519"),
+				),
+			},
+			{
+				Config:      fmt.Sprintf(configDataSourcePublicKeyViaOpenSSHPEM, "corrupt"),
+				ExpectError: regexp.MustCompile("ssh: no key found"),
+			},
+		},
+	})
+}
+
+func TestAccPublicKey_dataSource_NotBothPEM(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		Providers: testProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					data "tls_public_key" "test" {
+						private_key_pem = "does not matter"
+						private_key_openssh = "does not matter"
+					}
+				`,
+				ExpectError: regexp.MustCompile("either provide private key via `private_key_pem` or `private_key_openssh`, not both"),
+			},
+		},
+	})
 }
