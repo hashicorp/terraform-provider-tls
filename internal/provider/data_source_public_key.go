8ba85788eeb02c64c5ce32308a31000f9cb32731
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-03-15 17:14:53 +0000

    Updated data source `tls_public_key` to rely on the default template for `tfplugindocs`

--- internal/provider/data_source_public_key.go
+++ internal/provider/data_source_public_key.go
@@ -4,6 +4,7 @@ import (
 	"crypto"
 	"encoding/pem"
 	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"golang.org/x/crypto/ssh"
 )
@@ -12,7 +13,10 @@ func dataSourcePublicKey() *schema.Resource {
 	return &schema.Resource{
 		Read: readDataSourcePublicKey,
 
-		Description: "Get a public key from a PEM-encoded private key.",
+		Description: "Get a public key from a PEM-encoded private key.\n\n" +
+			"Use this data source to get the public key from a [PEM (RFC 1421)](https://datatracker.ietf.org/doc/html/rfc1421) " +
+			"or [OpenSSH PEM (RFC 4716)](https://datatracker.ietf.org/doc/html/rfc4716) formatted private key, " +
+			"for use in other resources.",
 
 		Schema: map[string]*schema.Schema{
 			"private_key_pem": {
@@ -54,7 +58,7 @@ func dataSourcePublicKey() *schema.Resource {
 				Description: "The public key, in  [OpenSSH PEM (RFC 4716)](https://datatracker.ietf.org/doc/html/rfc4716) format. " +
 					"This is also known as ['Authorized Keys'](https://www.ssh.com/academy/ssh/authorized_keys/openssh#format-of-the-authorized-keys-file) format. " +
 					"This is populated only if the configured private key is supported: this includes all `RSA` and `ED25519` keys, as well as `ECDSA` keys " +
-					"with curves `P256`, `P384` and `P521`; `ECDSA` with curve `P224` [is not supported](#limitations-ecdsa-with-p224-elliptic-curve).",
+					"with curves `P256`, `P384` and `P521`; `ECDSA` with curve `P224` [is not supported](../../).",
 			},
 
 			"public_key_fingerprint_md5": {
@@ -62,7 +66,7 @@ func dataSourcePublicKey() *schema.Resource {
 				Computed: true,
 				Description: "The fingerprint of the public key data in OpenSSH MD5 hash format, e.g. `aa:bb:cc:...`. " +
 					"Only available if the selected private key format is compatible, as per the rules for " +
-					"`public_key_openssh` and [ECDSA P224 limitations](#limitations-ecdsa-with-p224-elliptic-curve).",
+					"`public_key_openssh` and [ECDSA P224 limitations](../../).",
 			},
 
 			"public_key_fingerprint_sha256": {
@@ -70,7 +74,7 @@ func dataSourcePublicKey() *schema.Resource {
 				Computed: true,
 				Description: "The fingerprint of the public key data in OpenSSH SHA256 hash format, e.g. `SHA256:...`. " +
 					"Only available if the selected private key format is compatible, as per the rules for " +
-					"`public_key_openssh` and [ECDSA P224 limitations](#limitations-ecdsa-with-p224-elliptic-curve).",
+					"`public_key_openssh` and [ECDSA P224 limitations](../../).",
 			},
 
 			"id": {
