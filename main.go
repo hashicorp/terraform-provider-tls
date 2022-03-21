7de89295371de97c425d161e64b100662e8ace4f
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-03-21 12:09:22 +0000

    QoL: wiring up `tfplugindocs` to the code generation (#170)
    * Wiring up `tfplugindocs` to the Golang code generation
    
    A call to `go generate` will now generate the documentation for the provider.
    
    * Removing unused scripts
    
    * Upgrading `terraform-plugin-sdk/v2` from `v2.11.0` to `v2.12.0`
    
    * Simplifying Makefile (pr review)

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/terraform-providers/terraform-provider-tls/internal/provider"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: provider.New})
