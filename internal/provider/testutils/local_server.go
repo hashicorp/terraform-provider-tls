60459804a2950762751222b2c51e580be63f5c28
Ivan De Marino <ivan.demarino@hashicorp.com>
2022-06-27 13:10:15 +0100

    Added 'connection counters' to the `LocalServerTest`

--- internal/provider/testutils/local_server.go
+++ internal/provider/testutils/local_server.go
@@ -1,18 +1,23 @@
 package testutils
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 
 	"github.com/elazarl/goproxy"
 	"github.com/elazarl/goproxy/ext/auth"
+	r "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 // LocalServerTest is a simple HTTP server used for testing.
 type LocalServerTest struct {
-	listener net.Listener
-	server   *http.Server
+	listener      net.Listener
+	server        *http.Server
+	connActivated int
+	connClosed    int
 }
 
 // NewHTTPServer creates an HTTP server that listens on a random port.
@@ -22,48 +27,53 @@ func NewHTTPServer() (*LocalServerTest, error) {
 		return nil, err
 	}
 
-	return &LocalServerTest{
+	// Create HTTP server, listening on a randomly-selected port
+	localServer := &LocalServerTest{
 		listener: listener,
 		server: &http.Server{
 			Addr: listener.Addr().String(),
 		},
-	}, nil
+	}
+
+	// Count connections activated and closed
+	localServer.server.ConnState = func(conn net.Conn, state http.ConnState) {
+		if state == http.StateActive {
+			localServer.connActivated++
+		}
+		if state == http.StateClosed {
+			localServer.connClosed++
+		}
+	}
+
+	return localServer, nil
 }
 
 // NewHTTPProxyServer creates an HTTP Proxy server that listens on a random port.
 func NewHTTPProxyServer() (*LocalServerTest, error) {
-	listener, err := net.Listen("tcp", ":0")
+	localServer, err := NewHTTPServer()
 	if err != nil {
 		return nil, err
 	}
 
-	return &LocalServerTest{
-		listener: listener,
-		server: &http.Server{
-			Addr:    listener.Addr().String(),
-			Handler: goproxy.NewProxyHttpServer(),
-		},
-	}, nil
+	// Turn http server into a proxy
+	localServer.server.Handler = goproxy.NewProxyHttpServer()
+
+	return localServer, nil
 }
 
 // NewHTTPProxyServerWithBasicAuth creates an HTTP Proxy server that listens on a random port and expects HTTP Basic Auth.
 func NewHTTPProxyServerWithBasicAuth(expectedUsername, expectedPassword string) (*LocalServerTest, error) {
-	listener, err := net.Listen("tcp", ":0")
+	proxy, err := NewHTTPProxyServer()
 	if err != nil {
 		return nil, err
 	}
 
-	proxy := goproxy.NewProxyHttpServer()
-	proxy.OnRequest().HandleConnect(auth.BasicConnect("restricted", func(username, password string) bool {
+	// Add "HTTP Connect auth handler" to proxy server
+	proxy.server.Handler.(*goproxy.ProxyHttpServer).OnRequest().HandleConnect(auth.BasicConnect("restricted", func(username, password string) bool {
 		return username == expectedUsername && (expectedPassword == "" || password == expectedPassword)
 	}))
-	return &LocalServerTest{
-		listener: listener,
-		server: &http.Server{
-			Addr:    listener.Addr().String(),
-			Handler: proxy,
-		},
-	}, nil
+
+	return proxy, nil
 }
 
 // ServeTLS makes the server begin listening for TLS client connections.
@@ -94,4 +104,24 @@ func (lst *LocalServerTest) Close() error {
 
 func (lst *LocalServerTest) Address() string {
 	return lst.listener.Addr().String()
+}
+
+func (lst *LocalServerTest) ConnActivated() int {
+	return lst.connActivated
+}
+
+func (lst *LocalServerTest) ConnClosed() int {
+	return lst.connClosed
+}
+
+func TestCheckBothServerAndProxyWereUsed(server, proxy *LocalServerTest) r.TestCheckFunc {
+	return func(_ *terraform.State) error {
+		if server.ConnActivated() != proxy.ConnActivated() {
+			return fmt.Errorf("expected server and proxy actived connection count to match: server was %d, while proxy was %d", server.ConnActivated(), proxy.ConnActivated())
+		}
+		if server.ConnClosed() != proxy.ConnClosed() {
+			return fmt.Errorf("expected server and proxy closed connection count to match: server was %d, while proxy was %d", server.ConnClosed(), proxy.ConnClosed())
+		}
+		return nil
+	}
 }
