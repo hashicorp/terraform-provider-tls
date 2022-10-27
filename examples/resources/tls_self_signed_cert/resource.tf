b26689c915a4596227bd39d98111b335e73c8fa0
Benjamin Bennett <ben.bennett@hashicorp.com>
2022-10-27 16:48:28 +0100

    Removing changes to self-signed-cert example

--- examples/resources/tls_self_signed_cert/resource.tf
+++ examples/resources/tls_self_signed_cert/resource.tf
@@ -1,46 +1,16 @@
-#resource "tls_self_signed_cert" "example" {
-#  private_key_pem = file("private_key.pem")
-#
-#  subject {
-#    common_name  = "example.com"
-#    organization = "ACME Examples, Inc"
-#  }
-#
-#  validity_period_hours = 12
-#
-#  allowed_uses = [
-#    "key_encipherment",
-#    "digital_signature",
-#    "server_auth",
-#  ]
-#}
-
-#terraform {
-#  required_providers {
-#    tls = {
-#      source  = "hashicorp/tls"
-#      version = "3.4.0"
-#    }
-#  }
-#}
-
-resource "tls_private_key" "example" {
-  algorithm = "ECDSA"
-}
-
 resource "tls_self_signed_cert" "example" {
-  private_key_pem   = tls_private_key.example.private_key_pem
-  is_ca_certificate = true
+  private_key_pem = file("private_key.pem")
 
   subject {
-    organization = "Example"
+    common_name  = "example.com"
+    organization = "ACME Examples, Inc"
   }
 
-  # 3 Years
-  validity_period_hours = 24 * 365 * 3
+  validity_period_hours = 12
 
   allowed_uses = [
-    "cert_signing",
-    "crl_signing",
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
   ]
 }
