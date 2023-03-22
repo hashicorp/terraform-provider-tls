d3f1be291b575e8626451ade3d6a48d250f3746f
Brian Flad <bflad417@gmail.com>
2023-03-22 11:32:16 -0400

    all: Replace custom default value plan modifiers with framework default handling (#342)
    Now that terraform-plugin-framework version 1.2.0+ is in use, the provider code no longer needs to maintain custom plan modifiers for default values. This change switches those implementations to the built-in framework functionality.

--- internal/provider/resource_locally_signed_cert.go
+++ internal/provider/resource_locally_signed_cert.go
@@ -12,7 +12,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
@@ -22,7 +24,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 
 	"github.com/hashicorp/terraform-provider-tls/internal/provider/attribute_plan_modifier_bool"
-	"github.com/hashicorp/terraform-provider-tls/internal/provider/attribute_plan_modifier_int64"
 )
 
 type locallySignedCertResource struct{}
@@ -102,18 +103,16 @@ func (r *locallySignedCertResource) Schema(_ context.Context, req resource.Schem
 			"is_ca_certificate": schema.BoolAttribute{
 				Optional: true,
 				Computed: true,
+				Default:  booldefault.StaticBool(false),
 				PlanModifiers: []planmodifier.Bool{
 					boolplanmodifier.RequiresReplace(),
-					attribute_plan_modifier_bool.DefaultValue(types.BoolValue(false)),
 				},
 				Description: "Is the generated certificate representing a Certificate Authority (CA) (default: `false`).",
 			},
 			"early_renewal_hours": schema.Int64Attribute{
 				Optional: true,
 				Computed: true,
-				PlanModifiers: []planmodifier.Int64{
-					attribute_plan_modifier_int64.DefaultValue(types.Int64Value(0)),
-				},
+				Default:  int64default.StaticInt64(0),
 				Validators: []validator.Int64{
 					int64validator.AtLeast(0),
 				},
@@ -128,9 +127,9 @@ func (r *locallySignedCertResource) Schema(_ context.Context, req resource.Schem
 			"set_subject_key_id": schema.BoolAttribute{
 				Optional: true,
 				Computed: true,
+				Default:  booldefault.StaticBool(false),
 				PlanModifiers: []planmodifier.Bool{
 					boolplanmodifier.RequiresReplace(),
-					attribute_plan_modifier_bool.DefaultValue(types.BoolValue(false)),
 				},
 				Description: "Should the generated certificate include a " +
 					"[subject key identifier](https://datatracker.ietf.org/doc/html/rfc5280#section-4.2.1.2) (default: `false`).",
@@ -151,8 +150,8 @@ func (r *locallySignedCertResource) Schema(_ context.Context, req resource.Schem
 			},
 			"ready_for_renewal": schema.BoolAttribute{
 				Computed: true,
+				Default:  booldefault.StaticBool(false),
 				PlanModifiers: []planmodifier.Bool{
-					attribute_plan_modifier_bool.DefaultValue(types.BoolValue(false)),
 					attribute_plan_modifier_bool.ReadyForRenewal(),
 				},
 				Description: "Is the certificate either expired (i.e. beyond the `validity_period_hours`) " +
